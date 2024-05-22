19cfb8c3421303e9ea9a71f462ef2aae816edca4
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-05-21 22:12:49 -0500

    Update interfazV3.py

--- interfazV3.py
+++ interfazV3.py
@@ -3,11 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 from polygon import RESTClient
-from datetime import timedelta
-from datetime import datetime
+from datetime import timedelta, datetime
 import streamlit as st
 import io
 
+# Función para cargar datos desde un archivo local (en el repositorio de GitHub)
 def cargar_datos(filepath):
     data = pd.read_excel(filepath)
     data['date'] = pd.to_datetime(data['date'])
@@ -15,6 +15,7 @@ def cargar_datos(filepath):
     data = data.set_index('date')
     return data[['pred']]
 
+# Verificar si la opción existe para las fechas dadas
 def verificar_opcion(client, ticker, start_date, end_date):
     try:
         resp = client.get_aggs(ticker=ticker, multiplier=1, timespan="day", from_=start_date.strftime('%Y-%m-%d'), to=end_date.strftime('%Y-%m-%d'))
@@ -22,6 +23,7 @@ def verificar_opcion(client, ticker, start_date, end_date):
     except:
         return False
 
+# Obtener el historial de datos de una opción específica
 def obtener_historico(ticker_opcion, api_key, fecha_inicio, fecha_fin):
     client = RESTClient(api_key)
     resp = client.get_aggs(ticker=ticker_opcion, multiplier=1, timespan="day", from_=fecha_inicio.strftime('%Y-%m-%d'), to=fecha_fin.strftime('%Y-%m-%d'))
@@ -31,6 +33,7 @@ def obtener_historico(ticker_opcion, api_key, fecha_inicio, fecha_fin):
     df.index = df.index.date
     return df
 
+# Encontrar la opción más cercana dentro de un rango de días
 def encontrar_opcion_cercana(client, base_date, option_price, pred, option_days, option_offset, ticker):
     min_days = option_days - option_offset
     max_days = option_days + option_offset
@@ -44,14 +47,14 @@ def encontrar_opcion_cercana(client, base_date, option_price, pred, option_days,
             break
     return best_date
 
+# Realizar el backtesting con los parámetros dados
 def realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_allocation, fecha_inicio, fecha_fin, option_days=30, option_offset=0, close_to_close=False):
     data = cargar_datos(data_filepath)
     balance = balance_inicial
     resultados = []
     client = RESTClient(api_key)
-    
-    #fecha_inicio = pd.Timestamp(fecha_inicio)
-    #fecha_fin = pd.Timestamp(fecha_fin)
+    fecha_inicio = fecha_inicio.date()
+    fecha_fin = fecha_fin.date()
 
     for date, row in data.iterrows():
         if date < fecha_inicio or date > fecha_fin:
@@ -104,6 +107,7 @@ def realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_alloc
     resultados_df.to_excel('resultados_trades_1.xlsx')
     return resultados_df, balance
 
+# Graficar los resultados del backtesting
 def graficar_resultados(df, final_balance, balance_inicial):
     plt.figure(figsize=(14, 7))
     df['Ganancia acumulada'] = df['Resultado'].cumsum() + balance_inicial
@@ -121,24 +125,21 @@ def graficar_resultados(df, final_balance, balance_inicial):
     plt.savefig('resultados_backtesting.png')
     plt.show()
 
+# Función principal de la aplicación
 def main():
-    st.title("Backtesting ARKAD")
-    #st.write("Use this interface to set the values for 'option_days' and 'option_offset'.")
+    st.title("Option Days and Option Offset Setter")
+    st.write("Use this interface to set the values for 'option_days' and 'option_offset'.")
     
-    # Option Days input
-    option_days_input = st.number_input("**Option Days:** (Número de días de vencimiento de la opción que se está buscando durante el backtesting)", min_value=0, max_value=90, value=30, step=1)
-    
-    # Option Offset input
-    option_offset_input = st.number_input("**Option Offset:** (Rango de días de margen alrededor del número de días objetivo dentro del cual se buscará la opción más cercana)", min_value=0, max_value=90, value=7, step=1)
-    
-    # Additional inputs for the backtest function
+    # Inputs para los parámetros de la función de backtesting
+    option_days_input = st.number_input("Option Days:", min_value=0, max_value=90, value=30, step=1)
+    option_offset_input = st.number_input("Option Offset:", min_value=0, max_value=90, value=7, step=1)
     data_filepath = 'datos_8.xlsx'
     api_key = st.text_input("API Key", "tXoXD_m9y_wE2kLEILzsSERW3djux3an")
     ticker = st.text_input("Ticker Symbol", "SPY")
-    balance_inicial = st.number_input("Balance Inicial", min_value=0, value=100000, step=1000)
-    pct_allocation = st.number_input("Porcentaje de Asignación de Capital:", min_value=0.001, max_value=0.60, value=0.05)
-    fecha_inicio = st.date_input("Fecha de inicio del periodo de backtest:", min_value=datetime(2020, 1, 1))
-    fecha_fin = st.date_input("Fecha de finalización del periodo de backtest:", max_value=datetime.today())
+    balance_inicial = st.number_input("Initial Balance", min_value=0, value=100000)
+    pct_allocation = st.number_input("Percentage Allocation", min_value=0.0, max_value=1.0, value=0.05)
+    fecha_inicio = st.date_input("Start Date", value=pd.Timestamp("2024-01-01"))
+    fecha_fin = st.date_input("End Date", value=pd.Timestamp("2024-12-31"))
     trade_type = st.radio('Tipo de Operación', ('Close to Close', 'Open to Close'))
 
     if trade_type == 'Close to Close':
@@ -147,11 +148,8 @@ def main():
         close_to_close = False
     
     if st.button("Run Backtest"):
-        resultados_df, final_balance = realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_allocation, fecha_inicio, fecha_fin, option_days_input, option_offset_input, close_to_close)
-        st.success("Backtest ejecutado correctamente!")
-        st.write("Resultados:")
-        st.dataframe(resultados_df)
-        st.write(f"Balance final: ${final_balance:,.2f}")
+        resultados_df, final_balance = realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_allocation, pd.Timestamp(fecha_inicio), pd.Timestamp(fecha_fin), option_days_input, option_offset_input, close_to_close)
+        st.success("Backtest executed successfully!")
         
         # Provide download links for the generated files
         st.write("### Download Results")
@@ -159,27 +157,6 @@ def main():
         # Resultados DataFrame to Excel
         excel_buffer = io.BytesIO()
         resultados_df.to_excel(excel_buffer, index=False)
-        st.download_button(label="Download Excel Results", data=excel_buffer, file_name="resultados_trades_1.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-        
-        # Display and download the plot
-        st.write("### Plot")
-        fig, ax = plt.subplots(figsize=(14, 7))
-        resultados_df['Ganancia acumulada'] = resultados_df['Resultado'].cumsum() + balance_inicial
-        ax = resultados_df.set_index('Fecha')['Ganancia acumulada'].plot(kind='line', marker='o', linestyle='-', color='b', ax=ax)
-        ax.set_title(f'Resultados del Backtesting de Opciones - Balance final: ${final_balance:,.2f}')
-        ax.set_xlabel('Fecha')
-        ax.set_ylabel('Ganancia/Pérdida Acumulada')
-        plt.xticks(rotation=45)
-        ax.axhline(y=balance_inicial, color='r', linestyle='-', label='Balance Inicial')
-        plt.legend()
-        plt.grid(True, which='both', linestyle='-', linewidth=0.5)
-        plt.tight_layout()
-        img_buffer = io.BytesIO()
-        plt.savefig(img_buffer, format='png')
-        st.image(img_buffer)
-        st.download_button(label="Download Plot", data=img_buffer, file_name="resultados_backtesting.png", mime="image/png")
-
-if __name__ == "__main__":
-    main()
+        st.download_button(label="Download Excel Results", data=excel_buffer
 
 
