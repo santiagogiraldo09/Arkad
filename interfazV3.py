7b008d7c703c66494a2478e63376119751ad50f6
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-09-10 13:29:22 -0500

    Update interfazV3.py

--- interfazV3.py
+++ interfazV3.py
@@ -281,8 +281,8 @@ def main():
     fecha_fin = st.date_input("**Fecha de finalización del periodo de backtest:**", max_value=datetime.today())
     trade_type = st.radio('**Tipo de Operación**', ('Close to Close', 'Open to Close', 'Close to Open'))
     # Nuevos inputs para la hora de apertura y cierre
-    open_time = st.time_input("**Seleccionar Hora de Apertura:**", value=datetime.strptime("09:30", "%H:%M").time())
-    close_time = st.time_input("**Seleccionar Hora de Cierre:**", value=datetime.strptime("16:00", "%H:%M").time())
+    #open_time = st.time_input("**Seleccionar Hora de Apertura:**", value=datetime.strptime("09:30", "%H:%M").time())
+    #close_time = st.time_input("**Seleccionar Hora de Cierre:**", value=datetime.strptime("16:00", "%H:%M").time())
     periodo = st.radio("**Selecionar periodo de datos**", ('Diario','15 minutos'))
 
     #if trade_type == 'Close to Close':
