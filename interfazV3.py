79b812e8f77959efc00a1ca73f6cb2bed75cb4be
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-09-17 12:58:09 -0500

    Update interfazV3.py

--- interfazV3.py
+++ interfazV3.py
@@ -390,7 +390,7 @@ def main():
     #archivo_seleccionado_path = os.path.join(directorio_datos, archivo_seleccionado)
     
     #Toogle
-    on = st.toogle("Prueba")
+    on = st.toggle("Prueba")
     # Option Days input
     option_days_input = st.number_input("**Option Days:** (Número de días de vencimiento de la opción que se está buscando durante el backtesting)", min_value=0, max_value=90, value=30, step=1)
     
