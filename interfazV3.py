2a2d947bfe009d429d084a8766385e5911d1a8dd
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-09-18 12:16:31 -0500

    Update interfazV3.py

--- interfazV3.py
+++ interfazV3.py
@@ -345,8 +345,8 @@ def main():
             operation = {'CC': 'Close to Close', 'OC': 'Open to Close', 'CO': 'Close to Open'}.get(parts[0], 'Operación desconocida')
             info ={'Proba': 'Probabilidades', 'Pred': 'Predicciones'}.get(parts[1], 'Información desconocida')
             responsible = {'Valen': 'Valentina', 'Santi': 'Santiago', 'Andres': 'Andrés'}.get(parts[2], 'Responsable desconocido')
-            start_date = parts[2][2:4] + '/' + parts[3][4:6] #+ '/20' + parts[2][0:2]
-            end_date = parts[3][2:4] + '/' + parts[4][4:6] #+ '/20' + parts[3][0:2]
+            start_date = parts[3][2:4] + '/' + parts[3][4:6] #+ '/20' + parts[2][0:2]
+            end_date = parts[4][2:4] + '/' + parts[4][4:6] #+ '/20' + parts[3][0:2]
             version = parts[5].split('.')[0]
         except IndexError:
             return "Información desconocida del algoritmo", "Información desconocida del algoritmo", "Información desconocida del algoritmo", "Información desconocida del algoritmo", "Información desconocida del algoritmo"
