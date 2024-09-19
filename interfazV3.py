b2b25bfa63c0d0fbb14deb27f224280b8118a097
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-09-19 16:52:21 -0500

    Update interfazV3.py

--- interfazV3.py
+++ interfazV3.py
@@ -260,8 +260,12 @@ def main():
     directorio_datos = '.'
     archivos_disponibles = [archivo for archivo in os.listdir(directorio_datos) if archivo.endswith('.xlsx')]
     
+    #Toogle
+    toggle_activated = st.toggle("Se opera si se supera el Threshold")
+    column_name = 'toggle_true' if toggle_activated else 'toggle_false'
+    
     # Opción de selección del archivo .xlsx
-    data_filepath = st.selectbox("*Seleccionar archivo de datos históricos: (Trabajar en estos momentos con **modelo_andres_datos_act* el cual contiene datos desde 2022)", archivos_disponibles)
+    data_filepath = st.selectbox("*Seleccionar archivo de datos históricos:*", archivos_disponibles)
     #archivo_seleccionado = st.selectbox("Selecciona el archivo de datos:", archivos_disponibles)
     #archivo_seleccionado_path = os.path.join(directorio_datos, archivo_seleccionado)
     
