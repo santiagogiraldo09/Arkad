7c93d119790ad2fb984ab4a82f66a1ce2219c425
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-09-23 17:11:06 -0500

    Update interfazV3.py

--- interfazV3.py
+++ interfazV3.py
@@ -475,9 +475,11 @@ def main():
     #else:
         #close_to_close = False
 
+    #API KEY que se tenía
+    #tXoXD_m9y_wE2kLEILzsSERW3djux3an
     
     if st.button("Run Backtest"):
-        resultados_df, final_balance = realizar_backtest(data_filepath, 'tXoXD_m9y_wE2kLEILzsSERW3djux3an' , "SPY", balance_inicial, pct_allocation, pd.Timestamp(fecha_inicio), pd.Timestamp(fecha_fin), option_days_input, option_offset_input, trade_type, periodo, column_name)
+        resultados_df, final_balance = realizar_backtest(data_filepath, 'KCIUEY7RBRKTL8GI' , "SPY", balance_inicial, pct_allocation, pd.Timestamp(fecha_inicio), pd.Timestamp(fecha_fin), option_days_input, option_offset_input, trade_type, periodo, column_name)
         st.success("Backtest ejecutado correctamente!")
 
         # Guardar resultados en el estado de la sesión
