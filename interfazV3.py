961d6b48d93d6d8f7d32823c3c2066e5f6b54d4b
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-09-24 07:49:53 -0500

    Update interfazV3.py

--- interfazV3.py
+++ interfazV3.py
@@ -60,7 +60,7 @@ def get_open_and_close(ticker, api_av, fecha_inicio, fecha_fin):
         # Filtrar por rango de fechas
         df = df[(df.index >= fecha_inicio) & (df.index <= fecha_fin)]
         #st.write("DataFrame filtrado por rango de fechas:", df)
-        #st.write("Valores de Open y Close para el rango de fechas:", df_filtered[['open', 'close']])
+        st.write("Valores de Open y Close para el rango de fechas:", df[['open', 'close']])
         
         return df
     else:
