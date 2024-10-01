62b1a5320540f83396f8877c339edea02634842b
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-10-01 10:39:51 -0500

    Update interfazV3.py

--- interfazV3.py
+++ interfazV3.py
@@ -350,7 +350,7 @@ def realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_alloc
                 continue
         else: #periodo == '15 minutos'
             data_for_date = yf.download(ticker, start=date, end=date + pd.DateOffset(days=1))
-            data_for_date2 = obtener_historico_15min_pol(ticker, api_key, date, date + pd.timedelta(minutes=15))
+            data_for_date2 = obtener_historico_15min_pol(ticker, api_key, date, date + pd.Timedelta(minutes=15))
             #st.write(start)
             st.write(date)
             st.write(data_for_date)
