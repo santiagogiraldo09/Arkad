86db4dc9c5ea0710f8d04828dea283eb3a261c4f
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-10-01 19:22:49 -0500

    Update interfazV3.py

--- interfazV3.py
+++ interfazV3.py
@@ -441,7 +441,7 @@ def realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_alloc
         if option_date:
             option_type = 'C' if row[column_name] == 1 else 'P'
             option_name = f'O:{ticker}{option_date}{option_type}00{option_price}000'
-            option_name2 = f'O:{ticker}{option_date}{option_type}00{option_price2}000'
+            option_name2 = f'O:{ticker}{option_date2}{option_type}00{option_price2}000'
             st.write(option_name)
             st.write("option date2")
             st.write(option_date2)
@@ -459,7 +459,7 @@ def realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_alloc
                 df = get_open_and_close(ticker, api_av, fecha_inicio, fecha_fin)
                 #df_option2 = obtener_historico_15min_pol(ticker, api_key, date, date + timedelta(days=option_days))
                 #vo = verificar_opcion_15min(client, ticker, date, date + timedelta(days=option_days))
-                #vo = verificar_opcion_15min(client, ticker, fecha_inicio, fecha_fin)
+                vo = verificar_opcion_15min(client, ticker, fecha_inicio, fecha_fin)
                 #df_option2 = obtener_historico_15min_pol(option_name, api_key, date, date + timedelta(days=option_days))
                 #df2 = obtener_historico_15min_pol(option_name, api_key, date, date + timedelta(days=option_days))
                 st.write("df_option:")
@@ -468,8 +468,8 @@ def realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_alloc
                 st.dataframe(df)
                 #st.write("df_option2:")
                 #st.dataframe(df_option2)
-                #st.write("verificar opción 15min:")
-                #st.write(vo)
+                st.write("verificar opción 15min:")
+                st.write(vo)
                 #st.write("Respuesta JSON completa:", data)  # También se muestra en Streamlit
             if not df_option.empty:
                 if periodo == 'Diario':
