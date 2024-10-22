c1024845ccf40eeec2be5e06c2d3733f96887083
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-10-22 14:12:15 -0500

    Revert "Update interfazV3.py"
    This reverts commit f868c0a729282939f9f83f700a32e9fd1518cf83.

--- interfazV3.py
+++ interfazV3.py
@@ -501,7 +501,7 @@ def realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_alloc
                     #st.write(option_price)
                 option_date = encontrar_opcion_cercana(client, date, option_price, row[column_name], option_days, option_offset, ticker)
                 if option_date:
-                    st.dataframe(option_date)
+                    st.write(option_date)
                     option_type = 'C' if row[column_name] == 1 else 'P'
                     option_name = f'O:{ticker}{option_date}{option_type}00{option_price}000'
                     
