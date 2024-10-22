a5e74d89f960d4e9b95fafc2b3c8161e27a3d784
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-10-22 17:52:38 -0500

    Update interfazV3.py

--- interfazV3.py
+++ interfazV3.py
@@ -561,7 +561,7 @@ def realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_alloc
                             fecha_entrada = date
                             option_name_anterior = option_name
                             trade_result_anterior = trade_result
-                            num_contratos_anterior = num_contratos
+                            #num_contratos_anterior = num_contratos
                             etf_open_price_anterior = etf_open_price
                             etf_close_price_anterior = etf_close_price
                             # No registramos el resultado aún
