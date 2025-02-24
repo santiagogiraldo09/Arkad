07086af81aedee802b5712ae8c9662efcf88fef7
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2025-02-24 10:28:05 -0500

    Update interfazV3.py

--- interfazV3.py
+++ interfazV3.py
@@ -888,7 +888,7 @@ def realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_alloc
             else: #esce1 = False  
                 data_for_date = yf.download("SPY", start="2022-01-01", end=date + pd.DateOffset(days=1), multi_level_index=False, auto_adjust=False)
                 data_for_date = data_for_date.drop(data_for_date.index[-1])
-                data_for_date.columns = data_for_date.columns.str.lower()
+                #data_for_date.columns = data_for_date.columns.str.lower()
                 data_for_date.index.name = 'date'
                 print(data_for_date.columns)
                 if data_for_date.empty:
