c46b1761f5f515a0ee84dceb772fdbfa1e8719f6
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2025-03-06 17:24:39 -0500

    Update interfazV3.py

--- interfazV3.py
+++ interfazV3.py
@@ -912,7 +912,7 @@ def realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_alloc
                 st.write(datee)
                 st.write("date.index")
                 st.write(data_for_date3.index)
-                data_for_date3 = data_for_date3[data_for_date3.index >= datee]
+                data_for_date3 = data_for_date3[data_for_date3.iloc[72] >= datee]
                 st.write("datos con data_for_date3")
                 st.write(data_for_date3)
                 #st.write("datos eliminando ultimo index")
