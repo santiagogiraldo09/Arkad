434fee45efbdab297d2ca9e599c91c5f354d77da
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2025-07-15 16:12:50 -0500

    update

--- interfazV3.py
+++ interfazV3.py
@@ -600,11 +600,16 @@ def realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_alloc
                 #2. Extraer tiempos de entrada y salida del archivo
                 start_time = pd.to_datetime(row['start_time'])
                 start_time = start_time.tz_localize(colombia_tz).tz_convert(ny_tz)
+                start_time = start_time.tz_localize(None)
 
                 next_start_time = row[f'siguiente_{nombre_de_la_columna}']
                 next_start_time = next_start_time.tz_localize(colombia_tz).tz_convert(ny_tz)
+                next_start_time = next_start_time.tz_localize(None)
+                
                 end_time = pd.to_datetime(row['end_time'])
                 end_time = end_time.tz_localize(colombia_tz).tz_convert(ny_tz)
+                end_time = end_time.tz_localize(None)
+                
                 precio_usar_apertura = row['start_price']
                 precio_usar_cierre = row['end_price']
                 option_price = round(row['start_price'])
