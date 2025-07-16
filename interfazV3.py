1d094d32579ba783eb0eacc38807e3436e715ddb
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2025-07-15 19:45:33 -0500

    Update interfazV3.py

--- interfazV3.py
+++ interfazV3.py
@@ -715,6 +715,8 @@ def realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_alloc
                         
                         # Obtener el precio de apertura del ETF del índice para la fecha correspondiente con Yahoo Finance
                         etf_data = yf.download("SPY", start="2022-01-01", end=date + pd.Timedelta(days=1), multi_level_index=False, auto_adjust=False)
+                        st.write("etf_data")
+                        st.write(etf_data)
                         etf_data = etf_data.drop(etf_data.index[-1])
                         etf_data.columns = etf_data.columns.str.lower()
                         etf_data.index.name = 'date'
