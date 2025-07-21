dab1ca4efb3e880c0a7780a2cfe20d1ba36ef50b
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2025-07-21 15:17:58 -0500

    Update interfazV3.py

--- interfazV3.py
+++ interfazV3.py
@@ -567,7 +567,7 @@ def realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_alloc
         fecha_inicio = pd.Timestamp(fecha_inicio)
         fecha_fin = pd.Timestamp(fecha_fin)
 
-    if "Trades_H1" in data_filepath:
+    if "Trades_H1" or "Trades_H1_Best1" or "Trades_H1_Best2" or "Trades_H1_Best3"in data_filepath:
         nombre_de_la_columna = 'start_time'
         # Se crea la columna una sola vez, antes de recorrer
         data[f'siguiente_{nombre_de_la_columna}'] = data[nombre_de_la_columna].shift(-1)
