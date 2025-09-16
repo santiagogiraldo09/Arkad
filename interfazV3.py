5fd5920dcd9cc26214c9a3f758b1ec111537eeee
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2025-09-16 09:42:49 -0500

    Update interfazV3.py

--- interfazV3.py
+++ interfazV3.py
@@ -865,6 +865,22 @@ def realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_alloc
                             print(trade_result)
                         else:
                             st.write("No entró al end_time en df_option.index")
+                            # 1. Encontrar la última fecha disponible en el DataFrame
+                            ultima_fecha = df_option_end_time['fecha'].max().date()
+                            
+                            # 2. Filtrar el DataFrame para obtener solo los registros de esa última fecha
+                            df_ultima_fecha = df_option_end_time[df_option_end_time['fecha'].dt.date == ultima_fecha]
+                            
+                            # 3. Encontrar el registro a las 15:00:00 o la hora más cercana posterior
+                            # Crear un punto de referencia para las 15:00:00 del día
+                            punto_de_corte = pd.to_datetime(f'{ultima_fecha} 15:00:00')
+                            
+                            # Encontrar el primer registro que sea mayor o igual al punto de corte
+                            # Esto nos da la hora exacta a las 15:00:00 o la siguiente si no existe ese registro
+                            df_filtrado_corte = df_ultima_fecha[df_ultima_fecha['fecha'] >= punto_de_corte]
+                            st.write("df filtrado corte:")
+                            st.write(df_filtrado_corte)
+                            
         
         else: #El archivo no es Trades_H1
             if periodo == 'Diario':
