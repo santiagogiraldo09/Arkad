28324f20ff94c62e239797dca8413580758dfa85
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2025-09-29 08:27:34 -0500

    Update interfazV3.py

--- interfazV3.py
+++ interfazV3.py
@@ -778,22 +778,25 @@ def realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_alloc
                             #st.write("Precio de salida opción día de cierre:")
                             #st.write(option_close_price_cierre)
                             max_contract_value = option_open_price * 100
-                            #st.write(max_contract_value)
+                            st.write("max_contract_value")
+                            st.write(max_contract_value)
                             
                             if allocation_type == 'Porcentaje de asignación':
                                 st.write("Entra en este allocation_type por porcentaje de asignación")
                                 if next_start_time < end_time:
+                                    st.write("next_start_time < end_time")
                                     num_contratos = int((balance_posiciones * pct_allocation) / max_contract_value)
                                     st.write("balance_posiciones")
                                     st.write(balance_posiciones)
                                     st.write("num_contratos")
                                     st.write(num_contratos)
                                 else: #next_start_time > end_time:
+                                    st.write("next_start_time > end_time")
                                     num_contratos = int((balance * pct_allocation) / max_contract_value)
-                                    #st.write(balance)
-                                    #st.write(pct_allocation)
-                                    #st.write(max_contract_value)
-                                    #st.write(num_contratos)
+                                    st.write(balance)
+                                    st.write(pct_allocation)
+                                    st.write(max_contract_value)
+                                    st.write(num_contratos)
                             else: #allocation_type == 'Monto fijo de inversión':
                                 if balance < max_contract_value:
                                     #st.error("No hay suficiente dinero para abrir más posiciones. La ejecución del tester ha terminado.")
@@ -806,8 +809,8 @@ def realizar_backtest(data_filepath, api_key, ticker, balance_inicial, pct_alloc
                             #st.write("Option Type actual:")
                             #st.write(option_type)
                             cost_trade = max_contract_value * num_contratos
-                            #st.write("Costo de la operación:")
-                            #st.write(cost_trade)
+                            st.write("Costo de la operación:")
+                            st.write(cost_trade)
                             
                             if next_start_time < end_time:
                                 #st.write("Balance con posiciones abiertas:")
